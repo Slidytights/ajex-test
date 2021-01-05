2e67331d98eb8b6300171b7cb9598037018d6c16
Slidytights <mcmpaintingcontracts@gmail.com>
2021-01-05 21:22:51 +0000

    tabular data

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-var xhr = new XMLHttpRequest();
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 
-xhr.send();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    })
+
+    return`<td>${tableHeaders}</td>`;
+}
+
+
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+    getData(type, function(data) {
+        data = data.results;
+
+        data.forEach(function(item) {
+            
+           el.innerHTML += "<p>" + item.name + "</P>"; 
+        });        
+    });
+}
+
